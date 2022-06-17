e26c351905a4490da1271b38afe6e666ebe49b02
Guilherme <guilherme.gb@hotmail.com>
2022-06-17 10:37:38 -0300

    admin config

--- Django_Alura/anime/admin.py
+++ Django_Alura/anime/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Animes
 
 # Register your models here.
-admin.site.register(Animes)
+@admin.register(Animes)
 class AdminAnime(admin.ModelAdmin):
-    list_display:
+    list_display = ('idAnime', 'nomeAnime', 'dataLancamento')
