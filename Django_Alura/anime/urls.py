deb6979980724a51e6f7cdaa7f293d924443b301
guilherme <guilherme.gb@hotmail.com>
2022-06-24 14:15:45 -0300

    html

--- Django_Alura/anime/urls.py
+++ Django_Alura/anime/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('animes/home/', views.home, name='home'),
+    path('animes/form/', views.form, name='form'),
+    path('animes/save/', views.save, name='save'),
+    path('animes/list/', views.list, name='list'),
 ]
--- Django_Alura/anime/views.py
+++ Django_Alura/anime/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'animes/home.html')
+
+
+def form(request):
+    return render(request, 'animes/form.html')
+
+
+def save(request):
+    return render(request, 'animes/save.html')
+
+
+def list(request):
+    return render(request, 'animes/list.html')
